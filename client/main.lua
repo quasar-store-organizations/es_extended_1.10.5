d3ef3e55e97a6c1d973df1bf4903c7d9d36e8695
quasarstore <fernando.quasarstore@gmail.com>
2024-03-26 12:37:05 +0100

    Modified

--- client/main.lua
+++ client/main.lua
@@ -245,7 +245,7 @@ end)
 AddEventHandler('esx:restoreLoadout', function()
     ESX.SetPlayerData('ped', PlayerPedId())
 
-    if not Config.OxInventory then
+    if not Config.OxInventory and not Config.QSInventory then
         local ammoTypes = {}
         RemoveAllPedWeapons(ESX.PlayerData.ped, true)
 
@@ -310,7 +310,7 @@ AddEventHandler('esx:setAccountMoney', function(account)
     ESX.SetPlayerData('accounts', ESX.PlayerData.accounts)
 end)
 
-if not Config.OxInventory then
+if not Config.OxInventory and not Config.QSInventory then
     RegisterNetEvent('esx:addInventoryItem')
     AddEventHandler('esx:addInventoryItem', function(item, count, showNotification)
         for k, v in ipairs(ESX.PlayerData.inventory) do
@@ -378,7 +378,7 @@ AddEventHandler('esx:setJob', function(Job)
     ESX.SetPlayerData('job', Job)
 end)
 
-if not Config.OxInventory then
+if not Config.OxInventory and not Config.QSInventory then
     RegisterNetEvent('esx:createPickup')
     AddEventHandler('esx:createPickup', function(pickupId, label, coords, itemType, name, components, tintIndex)
         local function setObjectProperties(object)
@@ -429,7 +429,7 @@ AddEventHandler('esx:registerSuggestions', function(registeredCommands)
     end
 end)
 
-if not Config.OxInventory then
+if not Config.OxInventory and not Config.QSInventory then
     RegisterNetEvent('esx:removePickup')
     AddEventHandler('esx:removePickup', function(pickupId)
         if pickups[pickupId] and pickups[pickupId].obj then
@@ -440,7 +440,7 @@ if not Config.OxInventory then
 end
 
 function StartServerSyncLoops()
-    if not Config.OxInventory then
+    if not Config.OxInventory and not Config.QSInventory then
         -- keep track of ammo
 
         CreateThread(function()
@@ -484,7 +484,7 @@ if not Config.EnableWantedLevel then
     SetMaxWantedLevel(0)
 end
 
-if not Config.OxInventory then
+if not Config.OxInventory and not Config.QSInventory then
     CreateThread(function()
         while true do
             local Sleep = 1500
